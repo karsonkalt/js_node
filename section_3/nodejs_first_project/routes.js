33cf15542f24570b284cd5aad3bd17f14b5a53d0
Karson <kars.kalt@gmail.com>
2022-01-27 18:08:22 -0500

    Finished mod 4 on debugging

--- section_3/nodejs_first_project/routes.js
+++ section_3/nodejs_first_project/routes.js
@@ -28,12 +28,11 @@ function requestHandler(req, res) {
       console.log(chunk);
       body.push(chunk);
     });
-    req.on("end", () => {
+    return req.on("end", () => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split("=")[1];
       // .writeFile takes a callback which executes when the async is finished
       fs.writeFile("./message.txt", message, error => {
-        console.log(error);
         res.statusCode = 302;
         res.setHeader("Location", "/");
         return res.end();
--- section_4/notes.md
+++ section_4/notes.md
@@ -1,1 +1,3 @@
 # Debugging and NPM
+- `npm init`
+- Pass `--save-dev` into npm for development dependencies.
